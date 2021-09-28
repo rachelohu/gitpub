d37afba3b5db4d9dd835cd5c2c3909806b8779f0
rachelohu <rachelohu5@gmail.com>
2021-09-28 01:10:43 -0400

    changes

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const express = require('express');
 const app = express();
 //Import my drink data
 const drinks = require('./models/drinks');
+//Import my food data
+const food = require('./models/food');
 
 // INDEX ROUTE 
 app.get('/drinks', (req, res) => {
     res.render("drinks_index.ejs", {allDrinks: drinks});
   });
 
+app.get('/food', (req, res) => {
+    res.render("food_index.ejs", {allFood: food});
+  });
+
 // SHOW ROUTE
-app.get('/drinks/:indexOfDrinksArray', (req, res) => {
-    res.render("drinks_show.ejs", {drink: drinks[req.params.indexOfDrinksArray]});
+app.get('/drinks/:id', (req, res) => {
+    res.render("drinks_show.ejs", {drink: drinks[req.params.id]});
 });
+
+app.get('/food/:id', (req, res) => {
+    res.render("food_show.ejs", {item: food[req.params.id]});
+});
+
 
 //The Server Listener
 app.listen(3000, () => {
